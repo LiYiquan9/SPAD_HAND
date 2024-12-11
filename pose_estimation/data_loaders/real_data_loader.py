552b05b4821f21c5ac745f40d2bf8bc59b2e642a
Carter Sifferman <cpsiff@gmail.com>
2024-12-11 11:37:00 -0600

    ablate finetune dataset size experiment

--- pose_estimation/data_loaders/real_data_loader.py
+++ pose_estimation/data_loaders/real_data_loader.py
@@ -5,18 +5,32 @@ import numpy as np
 from pytorch3d.transforms.rotation_conversions import matrix_to_axis_angle
 import json
 import os
+import random
 
 # HAND_MEAN = torch.tensor([0.1116787156904433, -0.04289217484104638, 0.41644183681244606, 0.10881132513711185, 0.0659856788782267, 0.7562200100001023, -0.09639296514009964, 0.09091565922041477, 0.18845929069701614, -0.11809503934386674, -0.05094385260705537, 0.529584499976084, -0.14369840869244885, -0.05524170001527845, 0.7048571406917286, -0.01918291683986582, 0.09233684821597642, 0.3379135244947721, -0.4570329833266365, 0.19628394516488204, 0.6254575328442732, -0.21465237881086027, 0.06599828649166892, 0.5068942070038754, -0.36972435736649994, 0.06034462636097784, 0.07949022787634759, -0.14186969453172144, 0.08585263331718808, 0.6355282566897771, -0.3033415874850632, 0.05788097522832922, 0.6313892099233043, -0.17612088501838064, 0.13209307627166406, 0.37335457646458126, 0.8509642789706306, -0.2769227420650918, 0.09154806978240378, -0.49983943762160615, -0.026556472160458842, -0.05288087673273012, 0.5355591477841195, -0.0459610409551833, 0.2773580212595623])
 
 
 class RealGTDataset(Dataset):
-    def __init__(self, dset_paths, split="test", num_cameras=8):
+    def __init__(self, dset_paths, split="test", num_cameras=8, subsample_rate=1):
+        """
+        Args:
+            dset_paths (list): List of paths to the datasets to load
+            split (str): Which split to load, either "test" or "train"
+            num_cameras (int): Number of cameras to use
+            subsample_rate (int): Rate at which to subsample the data. A rate of 1 means all of the
+                test data is used. 0.5 uses half (the first half)
+        
+        Returns:
+            None
+        """
         self.split = split
         self.path = ""
         self.x_data = []
         self.labels = []
         self.num_cameras = num_cameras
         self.dset_paths = dset_paths
+
+        assert self.split in ["test", "train"]
 
         for dataset_path in self.dset_paths:
             labels_path = f"{dataset_path}/gt/labels_corrected.json"
@@ -26,100 +40,16 @@ class RealGTDataset(Dataset):
 
             hists_dataset_path = f"{dataset_path}/tof"
 
+            # in the labels, idxs 0-44 are pose_aa, 45-54 are shape, 55-57 are global
+            # translation, 58-60 are global rotation
             if self.split == "test":
-                for j in range(0, 50):
-                    if not os.path.exists(f"{hists_dataset_path}/{(j+1):06d}.json"):
-                        continue
-
-                    hists = json.load(open(f"{hists_dataset_path}/{(j+1):06d}.json"))
-
-                    hists_target = []
-                    for i in range(1, 9):
-                        response = np.array(hists[str(i)]["hists"][0][1:]).astype(np.float64)
-                        response = np.sum(response, axis=0)  # combine 9 sub-cameras
-                        response /= int(1500e3 * 9) * 1.0
-                        hists_target.append(torch.tensor(response))
-                    sensor_data = torch.stack(hists_target).cpu().numpy()
-
-                    self.x_data.append(sensor_data)
-
-                    j_string = f"{j+1:06d}"
-
-                    mano_params_pose = (
-                        (torch.from_numpy(np.array(label_data[j_string]["pose_aa"])))
-                        .reshape(1, 45)
-                        .cpu()
-                        .numpy()
-                    )
-                    mano_params_shape = (
-                        torch.tensor(label_data[j_string]["shape"]).reshape(1, 10).cpu().numpy()
-                    )
-                    global_trans = (
-                        torch.tensor(label_data[j_string]["hand_transl"])
-                        .reshape(1, 3)
-                        .cpu()
-                        .numpy()
-                    )
-                    global_rot = (
-                        torch.from_numpy(np.array(label_data[j_string]["wrist_aa"]).reshape(1, 3))
-                        .cpu()
-                        .numpy()
-                    )
-
-                    mano_params = np.concatenate(
-                        [mano_params_pose, mano_params_shape, global_trans, global_rot], axis=1
-                    )
-
-                    # in the labels, idxs 0-44 are pose_aa, 45-54 are shape, 55-57 are global 
-                    # translation, 58-60 are global rotation
-
-                    self.labels.append(mano_params)
-
-            else:
-                for j in range(50, 250):
-
-                    if not os.path.exists(f"{hists_dataset_path}/{(j+1):06d}.json"):
-                        continue
-
-                    hists = json.load(open(f"{hists_dataset_path}/{(j+1):06d}.json"))
-
-                    hists_target = []
-                    for i in range(1, 9):
-                        response = np.array(hists[str(i)]["hists"][0][1:]).astype(np.float64)
-                        response = np.sum(response, axis=0)  # combine 9 sub-cameras
-                        response /= int(1500e3 * 9) * 1.0
-                        hists_target.append(torch.tensor(response))
-                    sensor_data = torch.stack(hists_target).cpu().numpy()
-
-                    self.x_data.append(sensor_data)
-
-                    j_string = f"{j+1:06d}"
-                    mano_params_pose = (
-                        (torch.from_numpy(np.array(label_data[j_string]["pose_aa"])))
-                        .reshape(1, 45)
-                        .cpu()
-                        .numpy()
-                    )
-                    mano_params_shape = (
-                        torch.tensor(label_data[j_string]["shape"]).reshape(1, 10).cpu().numpy()
-                    )
-                    global_trans = (
-                        torch.tensor(label_data[j_string]["hand_transl"])
-                        .reshape(1, 3)
-                        .cpu()
-                        .numpy()
-                    )
-                    global_rot = (
-                        torch.from_numpy(np.array(label_data[j_string]["wrist_aa"]).reshape(1, 3))
-                        .cpu()
-                        .numpy()
-                    )
-
-                    mano_params = np.concatenate(
-                        [mano_params_pose, mano_params_shape, global_trans, global_rot], axis=1
-                    )
-
-                    self.labels.append(mano_params)
+                idxs = range(0, 50)
+                subsampled_idxs = random.sample(idxs, int(50 * subsample_rate))
+                self.load_data_idxs(subsampled_idxs, hists_dataset_path, label_data)
+            elif self.split == "train":
+                idxs = range(50, 250)
+                subsampled_idxs = random.sample(idxs, int(200 * subsample_rate))
+                self.load_data_idxs(subsampled_idxs, hists_dataset_path, label_data)
 
         if self.x_data == []:
             print(f"WARNING: No data found for dataset paths {self.dset_paths}, split {self.split}")
@@ -128,6 +58,49 @@ class RealGTDataset(Dataset):
         else:
             self.x_data = np.array(self.x_data)[:, :, :64]
             self.labels = np.array(self.labels)[:, 0, :]
+
+    def load_data_idxs(self, idxs, hists_dataset_path, label_data):
+        for j in idxs:
+            if not os.path.exists(f"{hists_dataset_path}/{(j+1):06d}.json"):
+                continue
+
+            hists = json.load(open(f"{hists_dataset_path}/{(j+1):06d}.json"))
+
+            hists_target = []
+            for i in range(1, 9):
+                response = np.array(hists[str(i)]["hists"][0][1:]).astype(np.float64)
+                response = np.sum(response, axis=0)  # combine 9 sub-cameras
+                response /= int(1500e3 * 9) * 1.0
+                hists_target.append(torch.tensor(response))
+            sensor_data = torch.stack(hists_target).cpu().numpy()
+
+            self.x_data.append(sensor_data)
+
+            j_string = f"{j+1:06d}"
+
+            mano_params_pose = (
+                (torch.from_numpy(np.array(label_data[j_string]["pose_aa"])))
+                .reshape(1, 45)
+                .cpu()
+                .numpy()
+            )
+            mano_params_shape = (
+                torch.tensor(label_data[j_string]["shape"]).reshape(1, 10).cpu().numpy()
+            )
+            global_trans = (
+                torch.tensor(label_data[j_string]["hand_transl"]).reshape(1, 3).cpu().numpy()
+            )
+            global_rot = (
+                torch.from_numpy(np.array(label_data[j_string]["wrist_aa"]).reshape(1, 3))
+                .cpu()
+                .numpy()
+            )
+
+            mano_params = np.concatenate(
+                [mano_params_pose, mano_params_shape, global_trans, global_rot], axis=1
+            )
+
+            self.labels.append(mano_params)
 
     def __len__(self):
         return self.x_data.shape[0]
--- pose_estimation/experiments/ablate_ft_dset_size.py
+++ pose_estimation/experiments/ablate_ft_dset_size.py
@@ -9,17 +9,18 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import yaml
+from tqdm import tqdm
 
 from pose_estimation.eval_real_data import eval_real_data
 from pose_estimation.finetune_real_data import finetune_real_data
 from pose_estimation.model import MANOEstimator
 
-PRETRAIN_MODEL_PATH = "pose_estimation/results/train/pt_sim/model_final.pth"
+PRETRAIN_MODEL_PATH = "pose_estimation/results/train/pt_sim_ft_all_but_carter/model_final.pth"
 OUTPUT_DIR = "pose_estimation/results/ablate_ft_dset_size"
 FT_DSET_PATH = "data/real_data/captures/2024-10-23_carter_250"
-FT_DSET_SIZES = [10, 25, 50]
-
+FT_DSET_SIZES = [1, 10, 20, 30, 40, 50]
+FULL_FT_DSET_SIZE = 200 # excluding test set
+NUM_TRIALS = 10
 
 def ablate_ft_dset_size() -> None:
 
@@ -30,38 +31,42 @@ def ablate_ft_dset_size() -> None:
     os.makedirs(output_subdir, exist_ok=True)
 
     # generate all the fine-tuned models with various fine-tuning dataset sizes
-    # for ft_dset_size in FT_DSET_SIZES:
-    #     this_run_output_dir = os.path.join(output_subdir, "train", f"{ft_dset_size}")
-    #     os.makedirs(this_run_output_dir, exist_ok=True)
+    for ft_dset_size in tqdm(FT_DSET_SIZES, desc="Fine-tuning models"):
+        for trial_num in range(NUM_TRIALS):
+            this_run_output_dir = os.path.join(output_subdir, "train", f"{ft_dset_size}", f"trial_{trial_num}")
+            os.makedirs(this_run_output_dir, exist_ok=True)
 
-    #     finetune_real_data(
-    #         dset_paths=[FT_DSET_PATH],
-    #         checkpoint_path=PRETRAIN_MODEL_PATH,
-    #         output_dir=this_run_output_dir,
-    #         epochs=500,
-    #         batch_size=16,
-    #         num_cameras=8,
-    #         save_model_interval=10e10,  # only save the final model
-    #         rot_type="6d",
-    #     )
+            finetune_real_data(
+                dset_paths=[FT_DSET_PATH],
+                checkpoint_path=PRETRAIN_MODEL_PATH,
+                output_dir=this_run_output_dir,
+                epochs=500,
+                batch_size=16,
+                num_cameras=8,
+                save_model_interval=10e10,  # only save the final model
+                rot_type="6d",
+                subsample_train_dset_rate=ft_dset_size / FULL_FT_DSET_SIZE,
+            )
 
     # run evaluation on all the models
     for ft_dset_size in FT_DSET_SIZES:
-        output_dir = os.path.join(output_subdir, "eval", f"{ft_dset_size}")
-        os.makedirs(output_dir, exist_ok=True)
-        eval_real_data(
-            checkpoint_path=os.path.join(
-                output_subdir, "train", f"{ft_dset_size}", "model_final.pth"
-            ),
-            eval_dataset_paths=[FT_DSET_PATH],
-            output_dir=output_dir,
-            rot_type="6d",
-            vis_results=False,
-        )
+        for trial_num in range(NUM_TRIALS):
+            output_dir = os.path.join(output_subdir, "eval", f"{ft_dset_size}", f"trial_{trial_num}")
+            os.makedirs(output_dir, exist_ok=True)
+            eval_real_data(
+                checkpoint_path=os.path.join(
+                    output_subdir, "train", f"{ft_dset_size}", f"trial_{trial_num}", "model_final.pth"
+                ),
+                eval_dataset_paths=[FT_DSET_PATH],
+                output_dir=output_dir,
+                rot_type="6d",
+                vis_results=False,
+            )
 
     # read in all the evaluation results and visualize them
     all_results = {
         "ft_dset_size": [],
+        "trial_num": [],
         "MPVPE": [],
         "MPJPE": [],
         "PA_MPVPE": [],
@@ -70,17 +75,37 @@ def ablate_ft_dset_size() -> None:
         "F@15mm": [],
     }
     for ft_dset_size in FT_DSET_SIZES:
-        with open(os.path.join(output_subdir, "eval", f"{ft_dset_size}", "results.json"), "r") as f:
-            results = json.load(f)
+        for trial_num in range(NUM_TRIALS):
+            with open(os.path.join(output_subdir, "eval", f"{ft_dset_size}", f"trial_{trial_num}", "results.json"), "r") as f:
+                results = json.load(f)
 
-        all_results["ft_dset_size"].append(ft_dset_size)
-        for k, v in results["test"].items():
-            all_results[k].append(v)
+            all_results["ft_dset_size"].append(ft_dset_size)
+            all_results["trial_num"].append(trial_num)
+            for k, v in results["test"].items():
+                all_results[k].append(v)
 
     all_results = pd.DataFrame(all_results)
-    sns.lineplot(data=all_results, x="ft_dset_size", y="PA_MPJPE")
+    for metric in ["MPVPE", "MPJPE", "PA_MPVPE", "PA_MPJPE", "F@5mm", "F@15mm"]:
+        fig, ax = plt.subplots()
 
-    plt.show()
+        # Calculate mean, min, and max for each ft_dset_size
+        summary = all_results.groupby("ft_dset_size").agg(
+            mean_metric=(metric, 'mean'),
+            min_metric=(metric, 'min'),
+            max_metric=(metric, 'max')
+        ).reset_index()
+
+        # Plot the average line
+        sns.lineplot(ax=ax, data=summary, x="ft_dset_size", y="mean_metric", marker='o', label='Average')
+
+        # Plot the shaded region for min and max
+        ax.fill_between(summary["ft_dset_size"], summary["min_metric"], summary["max_metric"], color='gray', alpha=0.3)
+
+        ax.set_xlabel("Fine-tuning dataset size")
+        ax.set_ylabel(metric)
+        plt.savefig(os.path.join(output_subdir, f"{metric}.png"))
+
+        plt.show()
 
 if __name__ == "__main__":
     ablate_ft_dset_size()
